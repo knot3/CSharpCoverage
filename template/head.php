8abe0b71c5ae803e87bbab2bb2b99e1187899379
PSE Knot³ <uaeti@student.kit.edu>
2014-02-09 18:43:52 +0100

    ...

---
+++ template/head.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Knot&sup3;</title>
+<style type="text/css">
+td {
+	vertical-align: top;
+}
+</style>
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,700">
+<link href='http://fonts.googleapis.com/css?family=Fauna+One' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/normalize.css">
+<link rel="stylesheet" href="/knot.css">
+</head>
+<body>
+
